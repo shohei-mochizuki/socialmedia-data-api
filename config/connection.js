889c5fd04ffb6ec817a313cf782c8390fca1d62d
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-11 12:38:55 -0500

    Create connection to socialMediaDB in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
 // OR mongodb://127.0.0.1:27017/socialMediaDB
-connect('mongodb://localhost/developersApplications', {
+connect('mongodb://localhost/socialMediaDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
