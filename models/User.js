0be7ff11eb475210d5f00c603519b9dad1d80971
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-11 13:40:53 -0500

    Finalize User model

--- models/User.js
+++ models/User.js
@@ -15,15 +15,27 @@ const userSchema = new Schema(
       unique: true,
       match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i
     },
-    thoughts: [],
-    friends: [],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+  {
     toJSON: {
       virtuals: true,
     },
     // NOT SURE!!!!
-    // id: false,
+    id: false,
   }
 );
 
@@ -33,12 +45,6 @@ userSchema
   // Getter
   .get(function () {
     return this.friends.length;
-  // })
-  // Setter to set the first and last name
-  // .set(function (v) {
-  //   const first = v.split(' ')[0];
-  //   const last = v.split(' ')[1];
-  //   this.set({ first, last });
   });
 
 // Initialize our User model
