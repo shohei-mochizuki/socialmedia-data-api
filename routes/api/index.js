dff86fee0a580c62961662668ac5afeb870b423f
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-13 17:43:33 -0400

    Update index.js for routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const appRoutes = require('./appRoutes');
 const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./throughtRoutes');
 
-router.use('/apps', appRoutes);
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
