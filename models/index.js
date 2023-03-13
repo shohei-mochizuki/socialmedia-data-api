703649879822a3bdeac22eb50970e0f87d1a2d40
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-13 17:40:58 -0400

    Update index.js in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 const User = require('./User');
-const Tag = require('./Tag');
-const Application = require('./Application');
+const Thought = require('./Thought');
 
-module.exports = { User, Application, Tag };
+module.exports = { User, Thought };
